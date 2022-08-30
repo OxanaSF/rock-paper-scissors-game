4bb60ccd7a5e52892a3f3f36d5825832deae4428
OxanaSF <ovtyutcheva@gmail.com>
2022-08-30 10:29:50 -0700

    added desktop layout for Modal

--- src/components/Rules/RulesModal.js
+++ src/components/Rules/RulesModal.js
@@ -6,9 +6,22 @@ import classes from './RulesModal.module.css';
 
 export const ModalOverlay = (props) => {
   const content = (
+    // <div className={classes.modal__overlay__container}>
     <div className={classes.modal__overlay} onClick={props.onClick}>
       <h1>RULES</h1>
+      <div className={classes.modal__header}>
+        <h2>RULES</h2>
+        <div className={classes.modal__close}>
+          <img
+            src={`${process.env.PUBLIC_URL}/images/icon-close.svg`}
+            alt="close"
+          />
+        </div>
+      </div>
+
+      <div className={classes.modal__overlay__image}></div>
     </div>
+    // </div>
   );
 
   return createPortal(content, document.getElementById('rules-modal-root'));
@@ -17,8 +30,8 @@ export const ModalOverlay = (props) => {
 const RulesModal = (props) => {
   return (
     <Fragment>
-      <BackdropModal  onClick={props.onClick}/>
-      <ModalOverlay onClick={props.onClick}/>
+      <BackdropModal onClick={props.onClick} />
+      <ModalOverlay onClick={props.onClick} />
     </Fragment>
   );
 };
